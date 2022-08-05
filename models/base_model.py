b4d9c8e99a47266226bcd573bb86ee4d9cd1f5d2
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-08-05 13:58:00 +0300

    Adding Kwargs and Args & Fixing errors

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,29 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = uuid4()
-        self.created_at = datetime.today().isoformat()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    self.__dict__[key] = value
+        else:
+            self.id = uuid4()
+            self.created_at = datetime.today().isoformat()
+            self.updated_at = self.created_at
 
     def save(self):
         self.updated_at = datetime.today().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
     def to_dict(self):
+        c_at = self.created_at.isoformat()
+        c_at = self.updated_at.isoformat()
+        self_d = self.__dict__
+        self_d["__class__"] = __class__.__name__
+        self_d["created_at"] = c_at
+        self_d["updated_at"] = u_at
         return self.__dict__
 
     def __str__(self):
-        return f"[<{__class__.__name__}>] (<{self.id}>) <{self.__dict__}>"
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
