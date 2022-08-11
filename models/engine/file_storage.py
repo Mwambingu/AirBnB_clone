cd6b28bdbf11b8bb141bd09166183f60afc21ed2
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-08-11 15:40:28 +0300

    Hot fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,6 @@ class FileStorage:
 
     def new(self, obj):
         """Adds a new instance to the object storage"""
-        print(obj)
         cls_id = "{}{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[cls_id] = obj
 
